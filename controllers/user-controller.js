ca7251419ea976f511ae639dfe2b031ceb40c7ef
Moradi1412 <moradi.javid@gmail.com>
2021-10-20 17:42:22 -0700

    update user added to controller and route

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -25,7 +25,21 @@ createUser({ body }, res) {
     User.create(body)
     .then(dbUserData => res.json(dbUserData))
     .catch(err => res.json(err));
-}
+}, 
+
+updateUser ({ params, body }, res ) { 
+    User.findOneAndUpdate({ _id : params.id }, body, { new: true } )
+    .then (dbUserData => {
+        if (!dbUserData) {
+        res.status(404).json({ message: 'No User found with this id!' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => res.json(err));
+},
+
+
 }; 
 
 module.exports = userController; 
--- routes/api/User-routes.js
+++ routes/api/User-routes.js
@@ -3,13 +3,15 @@ const router = require('express').Router();
 const { 
     getAllUser,
     createUser,
-    getUserById } = require ('../../controllers/user-controller'); 
+    getUserById, 
+    updateUser } = require ('../../controllers/user-controller'); 
 
 router.route('/')
 .get(getAllUser)
 .post(createUser); 
 
 router.route('/:id')
-.get(getUserById);
+.get(getUserById)
+.put(updateUser); 
 
 module.exports = router; 
